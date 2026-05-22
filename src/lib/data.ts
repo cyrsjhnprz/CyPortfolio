ff9f574b9b27a200844da1d5c40ceca15048bc7f
Cyrus John S. Perez <cyrsjhnprz04@gmail.com>
2026-05-22 19:48:23 +0800

    update message for demo

--- src/lib/data.ts
+++ src/lib/data.ts
@@ -56,8 +56,8 @@ export const projects = [
     description:
       "A secure user authentication system with login, registration, and password reset functionality.",
     stack: ["React", "Node.js", "Express", "MySQL"],
-    github: "https://github.com/yourusername/authentication-system",
-    demo: "https://authentication-system-demo.vercel.app"
+    github: "https://www.google.com/search?q=in+debugging+state&sca_esv=525e608177515078&sxsrf=ANbL-n6inLPzgv-O9qf4s_5oD_tO9ZUckg%3A1779450375638&ei=B0IQapvXJuWu2roPhaSz0AU&biw=2560&bih=1305&ved=0ahUKEwjb9qOc6cyUAxVll1YBHQXSDFoQ4dUDCBA&uact=5&oq=in+debugging+state&gs_lp=Egxnd3Mtd2l6LXNlcnAiEmluIGRlYnVnZ2luZyBzdGF0ZTIFECEYoAEyBRAhGKABMgUQIRifBTIFECEYnwUyBRAhGJ8FMgUQIRifBUieL1AAWM4ocAN4AZABAZgBkwGgAeEPqgEEMTQuN7gBA8gBAPgBAZgCF6AClg_CAgoQIxiABBiKBRgnwgILEAAYgAQYigUYkQLCAhAQABiABBiKBRhDGLEDGIMBwgIKEAAYgAQYigUYQ8ICChAuGIAEGIoFGEPCAgUQLhiABMICCxAuGIAEGLEDGIMBwgIIEAAYgAQYsQPCAg4QLhiABBixAxjHARjRA8ICBRAAGIAEwgIOEC4YgAQYigUYsQMYgwHCAg4QABiABBiKBRiRAhixA8ICBxAAGIAEGArCAgIQJsICBhAAGBYYHsICBhAAGB4YDcICCBAAGAgYHhgNwgILEAAYgAQYigUYhgPCAgUQABjvBcICCBAAGIAEGKIEwgIIEAAYFhgeGAqYAwDiAwUSATEgQJIHBDE1LjigB9uJAbIHBDEyLji4B44PwgcGMC4yMS4yyAc2gAgB&sclient=gws-wiz-serp",
+    demo: "https://www.google.com/search?q=not+avalable+for+demo&oq=not+avalable+for+demo&gs_lcrp=EgZjaHJvbWUyBggAEEUYOTIICAEQABgWGB4yCAgCEAAYFhgeMggIAxAAGBYYHjINCAQQABiGAxiABBiKBTIHCAUQABjvBTIHCAYQABjvBdIBCDU1OTFqMGo3qAIAsAIA&sourceid=chrome&ie=UTF-8"
   },
   {
     title: "FU BEAR Quiz App",
@@ -66,7 +66,7 @@ export const projects = [
       "A front-end quiz application with multiple-choice questions, score tracking, and a clean user interface.",
     stack: ["HTML", "CSS", "JavaScript"],
     github: "https://github.com/CjaySilvano/TE-quiz-app.git",
-    demo: "https://ecommerce-ui-demo.vercel.app"
+    demo: "https://www.google.com/search?q=not+avalable+for+demo&oq=not+avalable+for+demo&gs_lcrp=EgZjaHJvbWUyBggAEEUYOTIICAEQABgWGB4yCAgCEAAYFhgeMggIAxAAGBYYHjINCAQQABiGAxiABBiKBTIHCAUQABjvBTIHCAYQABjvBdIBCDU1OTFqMGo3qAIAsAIA&sourceid=chrome&ie=UTF-8"
   }
 ];
 
